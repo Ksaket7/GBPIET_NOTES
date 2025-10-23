a9740d936709e87145ceb87fbdf3e0a6cfecb35a
Saket Kandari <saket12kandari@gmail.com>
2025-10-23 11:50:36 +0530

    added user and notes model

---
+++ backend/src/models/note.model.js
@@ -0,0 +1,68 @@
+import mongoose, { Schema } from "mongoose";
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    fileUrl: {
+      type: String,
+      required: true,
+    },
+    uploadedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    originalStudent: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    upvotes: {
+      type: Number,
+      default: 0,
+    },
+    comments: [
+      {
+        user: { type: Schema.Types.ObjectId, ref: "User" },
+        message: { type: String },
+        createdAt: { type: Date, default: Date.now },
+      },
+    ],
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    subjectName: {
+      type: String,
+      trim: true,
+    },
+    subjectCode: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    type: {
+      type: String,
+      enum: ["notes", "pyqs", "tuts", "assignments"],
+      default: "notes",
+    },
+    tags: [
+      {
+        type: String,
+        lowercase: true,
+        trim: true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Note = mongoose.model("Note", noteSchema);
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,107 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: String,
+      enum: ["student", "cr", "faculty", "admin"],
+      default: "student",
+    },
+    branch: {
+      type: String,
+      trim: true,
+    },
+    year: {
+      type: String,
+      trim: true,
+    },
+    credits: {
+      type: Number,
+      default: 0,
+    },
+    upvotes: {
+      type: String,
+      default: 0,
+    },
+    coverImage: {
+      type: String,
+    },
+    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    follower: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+      role: this.role,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRrefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
